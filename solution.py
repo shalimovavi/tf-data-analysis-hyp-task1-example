55ce7f0c32a7da24bb896fe0ac2f552017082b0e
shalimovavi <45150630+shalimovavi@users.noreply.github.com>
2023-04-13 23:33:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1267315308 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  tab = np.array([[x_success, x_cnt - x_success], [y_success, y_cnt - y_success]])
+  p_v = st.chi2_contingency(table, correction=False)[1]
+  if (p_v < 0.02):
+    return True
+  else:
+    return False
