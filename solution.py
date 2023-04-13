9b29141a05774ba1d321218dd2e0979e0eedaa13
shalimovavi <45150630+shalimovavi@users.noreply.github.com>
2023-04-13 23:54:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,9 +8,9 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-  tab = np.array([[x_success, x_cnt - x_success], [y_success, y_cnt - y_success]])
-  p_v = st.chi2_contingency(table, correction=False)[1]
-  if (p_v < 0.02):
-    return True
-  else:
-    return False
+    tab = np.array([[x_success, x_cnt - x_success], [y_success, y_cnt - y_success]])
+    p_v = st.chi2_contingency(table, correction=False)[1]
+    if (p_v < 0.02):
+      return True
+    else:
+      return False
